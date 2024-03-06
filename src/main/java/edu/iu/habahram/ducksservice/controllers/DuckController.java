da8805fd6acdf2ce8a584bf04d5587737523f6e4
tannernovak <tannovak@iu.edu>
2024-03-06 16:37:01 -0500

    done

--- src/main/java/edu/iu/habahram/ducksservice/controllers/DuckController.java
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/DuckController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -36,7 +37,9 @@ public class DuckController {
     @GetMapping
     public List<DuckData> findAll() {
         try {
-            return ducksRepository.findAll();
+            List<DuckData> ducks = ducksRepository.findAll();
+            ducks.sort(Comparator.comparing(DuckData::type));
+            return ducks;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
